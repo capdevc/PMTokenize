1be72a05c243240979ac6b5c2261b55b24906060
Cristian Capdevila <ccapdevi@utk.edu>
2014-11-06 14:29:54 -0500

    changed library versions

--- project/PMWordCountBuild.scala
+++ project/PMWordCountBuild.scala
@@ -9,12 +9,12 @@ object PMWordCountBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       name := "PMWordCount",
       organization := "com.pyaanalytics",
-      version := "0.1-SNAPSHOT",
+      version := "0.1",
       scalaVersion := "2.10.4",
       // add other settings here
-      libraryDependencies += "org.apache.spark" %% "spark-core" % "1.0.1",
-      libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.4.0",
-      libraryDependencies += "org.mongodb" % "mongo-java-driver" % "2.12.4",
+      libraryDependencies += "org.apache.spark" %% "spark-core" % "1.1.0",
+      libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.2.0",
+      libraryDependencies += "org.mongodb" % "mongo-java-driver" % "2.11.4",
       retrieveManaged := true,
       resolvers += "Akka Repository" at "http://repo.akka.io/releases/")
   )
